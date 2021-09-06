7094294295d64a604fd10526733bfa184d939885
Rosita <rositabannert@hotmail.co.uk>
2021-09-06 18:33:56 +0100

    WIP basic functionality

--- src/Data.jsx
+++ src/Data.jsx
@@ -7,8 +7,8 @@ export const Data = () => {
   // display the data with the key of card state
   return (
     <div>
-      <button onClick={() => setCounter(counter + 1)}>next</button>
       <button onClick={() => setCounter(counter + -1)}>previous</button>
+      <button onClick={() => setCounter(counter + 1)}>next</button>
       <h1>{chineseToEnglish[counter].hanzi}</h1>
       <h1>{chineseToEnglish[counter].pinyin}</h1>
       <h2>{chineseToEnglish[counter].meaning}</h2>
