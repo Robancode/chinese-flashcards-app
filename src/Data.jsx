4465e3a05950916e04f24e104383708b18406644
Rosita <rositabannert@hotmail.co.uk>
2021-09-06 18:26:45 +0100

    init

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { chineseToEnglish } from "./data";
+
+export const Data = () => {
+  const [counter, setCounter] = useState(chineseToEnglish[0].key);
+
+  // display the data with the key of card state
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>next</button>
+      <button onClick={() => setCounter(counter + -1)}>previous</button>
+      <h1>{chineseToEnglish[counter].hanzi}</h1>
+      <h1>{chineseToEnglish[counter].pinyin}</h1>
+      <h2>{chineseToEnglish[counter].meaning}</h2>
+    </div>
+  );
+};
