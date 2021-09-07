95736f6aebc7f42100e47cca5beef406d5100754
Rosita <rositabannert@hotmail.co.uk>
2021-09-07 07:45:00 +0100

    styling like a post it

--- src/Data.tsx
+++ src/Data.tsx
@@ -5,12 +5,22 @@ import { Button as MaterialUiButton, Card } from '@material-ui/core';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
-export const CardContainer = styled.div`
-height: 20rem;
-background-color: #edf1f5;
+// sticky colors: #ff7eb9,#ff65a3,#7afcff,#feff9c,#fff740
+
+export const FlipCardAndButtonsContainer = styled.div`
 display: flex;
 flex-direction: column;
-padding: 4rem;
+align-self: center;
+height: 40rem;
+width: 50%;
+`
+
+export const CardContainer = styled.div`
+height: 20rem;
+background-color: #feff9c;
+display: flex;
+flex-direction: column;
+padding: 2rem;
 border-radius: 1rem;
 justify-content: center;
 `
@@ -22,21 +32,49 @@ export const ButtonsContainer = styled.div`
 `
 
 export const CardTitle = styled.p`
+justify-self: center;
+align-self: center;
 `
 
-export const CardContent = styled.h1``
+export const CardContent = styled.h1`
+flex-grow:2;
 
-export const CardContent2 = styled.h3``
+`
+
 
 export const TextSection = styled.div`
- display: flex;
+ padding: 2rem;  
+display: flex;
  flex-direction: row;
  justify-content: space-around;
- align-items: center
+ align-items: center;
 `
 
 export const Button = styled(MaterialUiButton)`
   flex-grow: 2
+`
+
+export const CardFront = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 1rem;
+ flex-grow:1;
+
+`
+
+export const SideLabel = styled.div`
+display: flex;
+justify-content: flex-end;
+align-items: flex-start;
+flex-grow:1;
+
+`
+
+export const Explanation = styled.h1`
+  flex-grow: 2;
+  padding: 2rem;
+  text-align: center;
+  overflow: scroll;
 `
 
 const initialState: number = chineseToEnglish[0].key - 1;
@@ -46,7 +84,7 @@ export const Data: () => JSX.Element = () => {
   const [front, setFront] = useState(true)
   // display the data with the key of card state
   return (
-    <>
+    <FlipCardAndButtonsContainer>
       <ButtonsContainer>
         <Button onClick={() => counter === 0 ? setCounter(0) : setCounter(counter + -1)}>
           <ArrowBackIosIcon />
@@ -57,31 +95,35 @@ export const Data: () => JSX.Element = () => {
       </ButtonsContainer>
       <CardContainer onClick={() => setFront(!front)}>
         {front === true ?
-          <div>
-            <TextSection>
+          <>
+            <SideLabel>
+              Front
+           </SideLabel>
+            <CardFront>
               <CardTitle>
                 Hanzi:
-               </CardTitle>
+            </CardTitle>
               <CardContent>
                 {chineseToEnglish[counter].hanzi}
               </CardContent>
-            </TextSection>
-            <TextSection>
-
               <CardTitle>
                 Pinyin:
-          </CardTitle>
+              </CardTitle>
               <CardContent>
                 {chineseToEnglish[counter].pinyin}
               </CardContent>
-            </TextSection>
-          </div>
-          : <TextSection>
-            <CardContent2>
+            </CardFront></>
+          :
+          <>
+            <SideLabel>
+              Back
+           </SideLabel>
+            <Explanation>
               {chineseToEnglish[counter].meaning}
-            </CardContent2>
-          </TextSection>}
+            </Explanation>
+          </>
+        }
       </CardContainer>
-    </>
+    </FlipCardAndButtonsContainer>
   );
 };
